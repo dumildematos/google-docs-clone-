84613fe5cb497e92a96f2187905161c096b7bf73
Dumilde Matos <dumildejosematos@gmail.com>
2022-03-19 01:05:58 +0100

    save documents

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,21 @@
+import { 
+  BrowserRouter as Router ,
+  Routes,
+  Route,
+  Navigate
+} from 'react-router-dom';
+import { v4 as uuidV4 } from 'uuid';
 import { TextEditor } from "./TextEditor";
 
 function App() {
-  return <TextEditor />;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" exact element={<Navigate to={`/documents/${uuidV4()}`} />}/>      
+        <Route path="/documents/:id" element={<TextEditor />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
--- server/server.js
+++ server/server.js
@@ -1,10 +1,44 @@
-const io = require('socket.io')(3001, {
-    cors: {
-        origin: 'http://localhost:3000',
-        methods: ['GET','POST']
-    }
-});
+const mongoose = require("mongoose")
+const Document = require("./Document")
 
-io.on('connection', socket => {
+mongoose.connect("mongodb://127.0.0.1:27017/google-docs-clone", {
+    useNewUrlParser: true, 
+    useUnifiedTopology: true 
+    }, err => {
+    if(err) throw err;
+    console.log('Connected to MongoDB!!!')
+})
 
-});
+const io = require("socket.io")(3001, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
+  },
+})
+
+const defaultValue = ""
+
+io.on("connection", socket => {
+  socket.on("get-document", async documentId => {
+    const document = await findOrCreateDocument(documentId)
+    socket.join(documentId)
+    socket.emit("load-document", document.data)
+
+    socket.on("send-changes", delta => {
+      socket.broadcast.to(documentId).emit("receive-changes", delta)
+    })
+
+    socket.on("save-document", async data => {
+      await Document.findByIdAndUpdate(documentId, { data })
+    })
+  })
+})
+
+async function findOrCreateDocument(id) {
+  if (id == null) return
+
+
+  const document = await Document.findById(id)
+  if (document) return document
+  return await Document.create({ _id: id, data: defaultValue })
+}
